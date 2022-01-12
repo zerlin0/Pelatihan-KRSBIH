eaccccf6f549a2e9fcdfbe3849d8ad3c2d065b85
Dwi Setia Fardhana <84776462+CallmeSetia@users.noreply.github.com>
2022-01-12 20:30:59 +0700

    Update dasar_opencv.py

--- 2022/Pelatihan-Divisi/ImageProcessing/Meet2/dasar_opencv.py
+++ 2022/Pelatihan-Divisi/ImageProcessing/Meet2/dasar_opencv.py
@@ -1,27 +1,54 @@
-# Dasar Video Capture
-
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
+# cap = cv.VideoCapture(0) # Wecam Utama
+kamera = cv.VideoCapture(0) # Wecam Utama
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+#
+# Cek Kondisi Kamera
+# if not kamera.isOpened():
+#     print("Cannot open camera")
+#     exit()
+#
+def resizeGambar(frame,faktor = 50):
+    lebar = frame.shape[1]
+    tinggi = frame.shape[0]
+
+    # percent by which the image is resized
+    scale_percent = faktor
+
+    # calculate the 50 percent of original dimensions
+    width = int(lebar * scale_percent / 100)
+    height = int(tinggi * scale_percent / 100)
+
+    # dsize
+    dsize = (width, height)
+    frame_resize = cv.resize(frame, dsize)
+    return frame_resize
+
 
 while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    kondisi, frame = kamera.read()
     # if frame is read correctly ret is True
-    if not ret:
+    if not kondisi:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # # Our operations on the frame come here
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', frame)
+
+    frame_kecil = resizeGambar(frame, 30)
+    cv.imshow('frame bgr', frame_kecil)
+
+    frame_filter = cv.bilateralFilter(frame_kecil, 10, 70, 70)
+
+    frame_gray = cv.cvtColor(frame_filter, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame gray', frame_gray)
+
+    edges = cv.Canny(frame_filter, 100, 100)
+    cv.imshow('canny edge',edges )
+
     if cv.waitKey(1) == ord('q'):
         break
+
 # When everything done, release the capture
-cap.release()
+kamera.release()
 cv.destroyAllWindows()
